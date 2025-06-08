9c137669887126e8fd4e3276a21ef338a1b31b74
Homercha <mihakozorog18@gmail.com>
2025-06-08 22:38:24 +0300

    5
    class

---
+++ libraryclass/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab05Lib
+{
+    public class Town
+    {
+        public string Name;
+        public string Country;
+        public string Region;
+        public int Population;
+        public double YearIncome;
+        public double Square;
+        public bool HasPort;
+        public bool HasAirport;
+        public double PopulationDensity
+        {
+            get
+            {
+                return GetPopulationDensity();
+            }
+        }
+        public double YearIncomePerInhabitant
+        {
+            get
+            {
+                return GetYearIncomePerInhabitant();
+            }
+        }
+        public double GetYearIncomePerInhabitant()
+        {
+            return (Population != 0) ? YearIncome / Population : 0;
+        }
+        public double GetPopulationDensity()
+        {
+            return (Square != 0) ? Population / Square : 0;
+        }
+    }
+}
